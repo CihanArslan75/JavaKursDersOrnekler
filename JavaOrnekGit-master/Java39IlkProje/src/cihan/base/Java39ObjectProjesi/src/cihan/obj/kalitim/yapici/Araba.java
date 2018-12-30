e13c107d578c9ac0dff79c8552a1b8251576e7d7
CihanArslan75 <45720112+CihanArslan75@users.noreply.github.com>
2018-12-30 13:20:22 +0300

    aaa

--- JavaOrnekGit-master/Java39IlkProje/src/cihan/base/Java39ObjectProjesi/src/cihan/obj/kalitim/yapici/Araba.java
+++ JavaOrnekGit-master/Java39IlkProje/src/cihan/base/Java39ObjectProjesi/src/cihan/obj/kalitim/yapici/Araba.java
@@ -5,11 +5,13 @@ public class Araba extends BinekArac {
 	
 	public Araba(){
 		super(4); // super ile parametreliyi çaðýrýyoruz. Bu olmazsa üst klasstaki parametresiz olaný çalýþtýrýrdý
+		//this(1) ; // yazarsak 12. satýrda ki parametreli araba constructor çaðýrýr.
 		System.out.println("5");
 	}
 	
 	public Araba (int hiz){
 		this();  //6.satýrdaki kendi classýndaki arabayý çaðýrýr.
+		//this(1); // yazarsak kendi kendini çaðýrýr ve sonsuz döngüye girer.
 		System.out.println("6");
 	}
 
